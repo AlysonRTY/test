3efb919250b4885e3dc6b705e532600d55612375
AlysonRTYgit config --global user.email variationsforpiano@gmail.com <mula@mac.speedport.ip>
2025-02-07 15:32:15 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+console.log("the first war aginst javascript....");
+// Exercise 1
+let myName = "Max";
+console.log(myName);
+// 
+//  Exercise 2
+const age = 26;
+console.log(age);
+// 
+// Exercise 3
+const juliaAge = 32;
+const ageDiff = (age - juliaAge);
+console.log(ageDiff);
+// 
+// Exercise 4
+if (age > 21) {
+    agegap = "You are older than 21";
+} else {
+    agegap = "You are younger than 21";
+}
+
+console.log(agegap);
+// 
+// Exercise 5
+if (age < juliaAge) {
+    agegap2 = "Julia is older than you"
+} else if (age = juliaAge) {
+    agegap2 = "You have the same age as Julia"
+} else {
+    agegap2 = "Julia is younger than you"
+}
+console.log(agegap2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let person = {
+    name: "Peter",
+    age: 26,
+    height: 195,
+    eyeColor: "blue"
+};
+console.log(person);
+
+person["name"] = "John";
+console.log(person.name);
+let colors = ["red", "blue", "green", "purple"];
+colors[4] = 1;
+console.log(colors);
+
+
+
